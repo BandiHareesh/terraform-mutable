6356a0044e64d423a99852e8e955da20f2618ec8
Bandi Hareesh <bandihareesh7@gmail.com>
2021-12-21 06:06:23 +0530

    terraform-mutable

---
+++ databases/data.tf
@@ -0,0 +1,16 @@
+data "aws_ami" "ami" {
+  most_recent      = true
+  name_regex       = "^base-image"
+  //name_regex       = "^Centos*"
+  //owners           = ["973714476881"]
+  owners           = ["self"]
+}
+
+data "terraform_remote_state" "vpc" {
+  backend             = "s3"
+  config              = {
+    bucket            = "terraform-b57"
+    key               = "mutable/vpc/${var.ENV}/terraform.tfstate"
+    region            = "us-east-1"
+  }
+}
