4ebec1d89fe94c6805cee400a44f1e3e43fa2d27
Bandi Hareesh <bandihareesh7@gmail.com>
2021-12-21 06:02:26 +0530

    terraform-mutable

---
+++ vpc/vars.tf
@@ -0,0 +1,9 @@
+variable "VPC_PRIVATE_CIDR"         {}
+variable "VPC_PUBLIC_CIDR"          {}
+variable "ENV"                      {}
+variable "PRIVATE_SUBNET_CIDRS"     {}
+variable "PUBLIC_SUBNET_CIDRS"      {}
+variable "DEFAULT_VPC_ID"           {}
+variable "DEFAULT_VPC_CIDR"         {}
+variable "INTERNAL_DNS_ZONE_ID"     {}
+variable "SUBNETS_AZ"               {}
